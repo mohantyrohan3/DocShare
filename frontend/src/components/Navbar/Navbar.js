42ea878082787699bff5e858196f58d4b528a491
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-06-21 21:49:39 +0530

    navbar updated

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -6,15 +6,15 @@ import "./Navbar.css"
 export default function Navbar() {
   return (
     <div className='navbar-body'>
-        <Grid container spacing={2} >
+        <Grid container spacing={2} alignContent={'center'} >
 
                 {/* SideBar */}
                 <Grid item xs={2} justifyItems="center" alignItems="center">
-                    <MenuIcon sx={{cursor:'pointer', marginLeft:'1rem'}} onClick={()=>{console.log('Hello')}}/>
+                    <MenuIcon sx={{cursor:'pointer', marginLeft:'1rem' , marginTop:'0.6rem'}} onClick={()=>{console.log('Hello')}}/>
                 </Grid>
                 
                 {/* Title  */}
-                <Grid item xs={8} justifyItems="center" alignItems="center" sx={{textAlign:'center'}} className='navbar-title'>
+                <Grid item xs={8} justifyItems="center" sx={{textAlign:'center'}} className='navbar-title'>
                     DOCSHARE
                 </Grid>
         </Grid>
