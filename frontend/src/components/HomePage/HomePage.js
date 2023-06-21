dd3db32ad6f5b84b00016f6d619e3ca8e240f343
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-06-21 19:32:38 +0530

    Home Page Grid Completed

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -4,6 +4,14 @@ import Grid from '@mui/material/Grid';
 import { Container } from '@mui/material';
 import animationData from "../../assets/8558-floating-document.json"
 import Lottie from "lottie-react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+
 
 
 export default function HomePage() {
@@ -12,14 +20,30 @@ export default function HomePage() {
         
 
         <Container className='homepage-grid' maxWidth="false">
-            <Grid container  justifyContent={'space-between'}> 
+            <Grid container  justifyContent={'space-around'}> 
 
-                    <Grid item xs={5}>
+                    <Grid item xs={4}>
                         <div className='homepage-grid-div'>
-                            Content
+                        <Card sx={{ }} className='homepage-grid-card'>
+                            <CardContent>
+                                <Typography className='homepage-grid-card-content' gutterBottom variant="h5" component="div">
+                                SHARE DOCS SECURELY
+                                </Typography>
+                                <Typography className='homepage-grid-card-content' gutterBottom variant="h5" component="div">
+                                WITH FAMILY
+                                </Typography>
+                                <Typography className='homepage-grid-card-content' variant="body2" color="text.secondary">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Share</Button>
+                            </CardActions>
+                            </Card>  
                         </div>
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={4}>
                         <div className='homepage-grid-div'>
                         <Lottie animationData={animationData} loop={true} />;
                         </div>
